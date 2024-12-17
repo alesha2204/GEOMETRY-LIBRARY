973cfcf34ba0b2e126b7e7f6c27dc9790ac7db3d
alesha2204 <167609687+alesha2204@users.noreply.github.com>
2024-12-18 01:36:46 +0300

    Add files via upload

---
+++ GeometryLibrary/GeometryLibrary/geometry.cpp
@@ -0,0 +1,36 @@
+﻿#include "geometry.h"
+#include <cmath>
+
+const double PI = 3.141592653589793;
+
+namespace Geometry {
+
+    double squareArea(double side) {
+        return side * side;
+    }
+
+    double squarePerimeter(double side) {
+        return 4 * side;
+    }
+
+    double circleArea(double radius) {
+        return PI * radius * radius;
+    }
+
+    double circlePerimeter(double radius) {
+        return 2 * PI * radius;
+    }
+
+    double triangleArea(double base, double height) {
+        return 0.5 * base * height;
+    }
+
+    double trianglePerimeter(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    std::vector<std::string> listShapes() {
+        return { "Square", "Circle", "Triangle" };
+    }
+
+}
---
+++ GeometryLibrary/GeometryLibrary/geometry.h
@@ -0,0 +1,23 @@
+﻿#ifndef GEOMETRY_H
+#define GEOMETRY_H
+#include <string>
+#include <vector>
+
+namespace Geometry {
+
+    // Функции для расчета площади и периметра квадрата
+    double squareArea(double side);
+    double squarePerimeter(double side);
+
+    // Функции для расчета площади и периметра круга
+    double circleArea(double radius);
+    double circlePerimeter(double radius);
+
+    // Функции для расчета площади и периметра треугольника
+    double triangleArea(double base, double height);
+    double trianglePerimeter(double a, double b, double c);
+
+    std::vector<std::string> listShapes();
+}
+
+#endif // GEOMETRY_H
